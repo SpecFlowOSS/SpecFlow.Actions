ed8fa1430f676035ef11c09d4836e4d0d68ff8b5
Jordan Western <j.western@tricentis.com>
2021-08-03 15:34:02 +0100

    added missing method from interface WaitAndReturnElements from IBrowserInteractions

--- Plugins/SpecFlow.Actions.Selenium/SpecFlow.Actions.Selenium/BrowserInteractions.cs
+++ Plugins/SpecFlow.Actions.Selenium/SpecFlow.Actions.Selenium/BrowserInteractions.cs
@@ -8,6 +8,7 @@ namespace SpecFlow.Actions.Selenium
     public interface IBrowserInteractions
     {
         IWebElement WaitAndReturnElement(By elementLocator);
+        IEnumerable<IWebElement> WaitAndReturnElements(By elementLocator);
         void GoToUrl(string url);
         string GetUrl();
         T? WaitUntil<T>(Func<T> getResult, Func<T, bool> isResultAccepted) where T : class;
