16edcfaa49e7bafa935d70c10b5ebecb05386f0f
Jordan Western <j.western@tricentis.com>
2021-08-05 10:59:54 +0100

    Created Specflow.Actions.Browserstack project

---
+++ Plugins/Sepcflow.Actions.Browserstack/Sepcflow.Actions.Browserstack/BrowserstackRuntimePlugin.cs
@@ -0,0 +1,21 @@
+﻿using Specflow.Actions.Browserstack;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly:RuntimePlugin(typeof(BrowserstackRuntimePlugin))]
+
+namespace Specflow.Actions.Browserstack
+{
+    public class BrowserstackRuntimePlugin
+    {
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters,
+            UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            runtimePluginEvents.CustomizeScenarioDependencies += RuntimePluginEvents_CustomizeScenarioDependencies;
+        }
+
+        private void RuntimePluginEvents_CustomizeScenarioDependencies(object sender, CustomizeScenarioDependenciesEventArgs e)
+        {
+        }
+    }
+}
