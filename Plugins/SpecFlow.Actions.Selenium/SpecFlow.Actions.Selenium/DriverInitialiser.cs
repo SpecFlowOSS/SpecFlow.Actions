8586f779710036a26d6d6073c8529c1d5ac98f3f
Jordan Western <j.western@tricentis.com>
2021-08-05 14:03:06 +0100

    fixed incorrect resolution, added summary to GetInitialiser

--- Plugins/SpecFlow.Actions.Selenium/SpecFlow.Actions.Selenium/DriverInitialiser.cs
+++ Plugins/SpecFlow.Actions.Selenium/SpecFlow.Actions.Selenium/DriverInitialiser.cs
@@ -15,12 +15,17 @@ namespace SpecFlow.Actions.Selenium
         {
             _container = container;
 
-            _container.RegisterTypeAs<EdgeDriverInitialiser, IDriverInitialiser>(Browser.Firefox.ToString());
+            _container.RegisterTypeAs<EdgeDriverInitialiser, IDriverInitialiser>(Browser.Edge.ToString());
             _container.RegisterTypeAs<FirefoxDriverInitialiser, IDriverInitialiser>(Browser.Firefox.ToString());
             _container.RegisterTypeAs<ChromeDriverInitialiser, IDriverInitialiser>(Browser.Chrome.ToString());
             _container.RegisterTypeAs<InternetExplorerDriverInitialiser, IDriverInitialiser>(Browser.InternetExplorer.ToString());
         }
 
+        /// <summary>
+        /// Resolves a driver initialiser instance based on the specified browser
+        /// </summary>
+        /// <param name="browser"></param>
+        /// <returns></returns>
         public IDriverInitialiser GetInitialiser(Browser browser)
         {
             return _container.Resolve<IDriverInitialiser>(browser.ToString());
