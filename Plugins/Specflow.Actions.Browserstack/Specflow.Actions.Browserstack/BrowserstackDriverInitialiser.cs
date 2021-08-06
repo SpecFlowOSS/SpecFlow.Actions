39b5b57fc916e9c1a30f294f23de4170b0f2457b
Jordan Western <j.western@tricentis.com>
2021-08-06 12:01:03 +0100

    completed BrowserstackDriverInitialiser implementation

--- Plugins/Specflow.Actions.Browserstack/Specflow.Actions.Browserstack/BrowserstackDriverInitialiser.cs
+++ Plugins/Specflow.Actions.Browserstack/Specflow.Actions.Browserstack/BrowserstackDriverInitialiser.cs
@@ -1,4 +1,9 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
 using SpecFlow.Actions.Selenium;
 using System;
 using System.Collections.Generic;
@@ -7,24 +12,48 @@ namespace Specflow.Actions.Browserstack
 {
     public class BrowserstackDriverInitialiser : IDriverInitialiser
     {
+        private readonly Uri _browserstackRemoteServer;
+        private const string BrowserstackOptions = "bstack:option";
+
+        public BrowserstackDriverInitialiser()
+        {
+            _browserstackRemoteServer = new Uri("https://hub-cloud.browserstack.com/wd/hub/");
+        }
+
         public IWebDriver GetChromeDriver(Dictionary<string, string>? capabilities = null, string[]? args = null)
         {
-            throw new NotImplementedException();
+            var options = new ChromeOptions();
+
+            options.AddAdditionalCapability(BrowserstackOptions, capabilities);
+
+            return new RemoteWebDriver(_browserstackRemoteServer, options);
         }
 
         public IWebDriver GetEdgeDriver(Dictionary<string, string>? capabilities = null, string[]? args = null)
         {
-            throw new NotImplementedException();
+            var options = new EdgeOptions();
+
+            options.AddAdditionalCapability(BrowserstackOptions, capabilities);
+
+            return new RemoteWebDriver(_browserstackRemoteServer, options);
         }
 
         public IWebDriver GetFirefoxDriver(Dictionary<string, string>? capabilities = null, string[]? args = null)
         {
-            throw new NotImplementedException();
+            var options = new FirefoxOptions();
+
+            options.AddAdditionalCapability(BrowserstackOptions, capabilities);
+
+            return new RemoteWebDriver(_browserstackRemoteServer, options);
         }
 
         public IWebDriver GetInternetExplorerDriver(Dictionary<string, string>? capabilities = null, string[]? args = null)
         {
-            throw new NotImplementedException();
+            var options = new InternetExplorerOptions();
+
+            options.AddAdditionalCapability(BrowserstackOptions, capabilities);
+
+            return new RemoteWebDriver(_browserstackRemoteServer, options);
         }
     }
 }
