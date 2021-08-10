872e3c95644b8606fd502283c1d9c0e817283d7d
Jordan Western <j.western@tricentis.com>
2021-08-10 13:23:29 +0100

    Project creation

---
+++ Plugins/SpecFlow.Actions.Playwright/SpecFlow.Actions.Playwright/PlaywrightRuntimePlugin.cs
@@ -0,0 +1,22 @@
+﻿using SpecFlow.Actions.Playwright;
+using System;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly: RuntimePlugin(typeof(PlaywrightRuntimePlugin))]
+
+namespace SpecFlow.Actions.Playwright
+{
+    public class PlaywrightRuntimePlugin : IRuntimePlugin
+    {
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            runtimePluginEvents.CustomizeScenarioDependencies += RuntimePluginEvents_CustomizeScenarioDependencies;
+        }
+
+        private void RuntimePluginEvents_CustomizeScenarioDependencies(object sender, CustomizeScenarioDependenciesEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
