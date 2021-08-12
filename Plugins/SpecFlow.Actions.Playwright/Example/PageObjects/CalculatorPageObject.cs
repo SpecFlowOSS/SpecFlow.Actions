8fa64552b2e21984782c86119dd3f05ffe18f12e
Jordan Western <j.western@tricentis.com>
2021-08-12 16:07:58 +0100

    abstracted the Interactions away from the steps

--- Plugins/SpecFlow.Actions.Playwright/Example/PageObjects/CalculatorPageObject.cs
+++ Plugins/SpecFlow.Actions.Playwright/Example/PageObjects/CalculatorPageObject.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Playwright;
-using SpecFlow.Actions.Playwright;
+﻿using SpecFlow.Actions.Playwright;
 using System.Threading.Tasks;
 
 namespace Example.PageObjects
@@ -7,7 +6,7 @@ namespace Example.PageObjects
     /// <summary>
     /// Calculator Page Object
     /// </summary>
-    public class CalculatorPageObject
+    public class CalculatorPageObject : BasePage
     {
         // The page URL
         private protected const string CalculatorUrl = "https://specflowoss.github.io/Calculator-Demo/Calculator.html";
@@ -19,41 +18,29 @@ namespace Example.PageObjects
         private static string ResultLabelSelector => "#result";
         private static string ResetButtonSelector => "#reset-button";
 
-        private readonly Task<IPage> _page;
+        private Interactions _interactions;
 
-        public CalculatorPageObject(BrowserDriver browserDriver)
+        public CalculatorPageObject(BrowserDriver browserDriver) : base(browserDriver)
         {
-            _page = CreatePageAsync(browserDriver.Current);
-        }
-
-        private async Task<IPage> CreatePageAsync(Task<IBrowser> browser)
-        {
-            // Creates a new page instance
-            return await (await browser).NewPageAsync();
-        }
-
-        public async Task GoToPage()
-        {
-            // Opens the calculator page
-            await (await _page).GotoAsync(CalculatorUrl);
+            _interactions = new Interactions(_page);
         }
 
         public async Task EnterFirstNumberAsync(string number)
         {
             //Enter text
-            await FirstNumberFieldSelector.SendTextAsync(await _page, number);
+            await _interactions.SendTextAsync(FirstNumberFieldSelector, number);
         }
 
         public async Task EnterSecondNumberAsync(string number)
         {
             //Enter text
-            await SecondNumberFieldSelector.SendTextAsync(await _page, number);
+            await _interactions.SendTextAsync(SecondNumberFieldSelector, number);
         }
 
         public async Task ClickAddAsync()
         {
             //Click the add button
-            await AddButtonSelector.ClickAsync(await _page);
+            await _interactions.ClickAsync(AddButtonSelector);
         }
 
         public async Task EnsureCalculatorIsOpenAndResetAsync()
@@ -61,13 +48,13 @@ namespace Example.PageObjects
             //Open the calculator page in the browser if not opened yet
             if ((await _page).Url != CalculatorUrl)
             {
-                await GoToPage();
+                await _interactions.GoToUrl(CalculatorUrl);
             }
             //Otherwise reset the calculator by clicking the reset button
             else
             {
                 //Click the rest button
-                await ResetButtonSelector.ClickAsync(await _page);
+                await _interactions.ClickAsync(ResetButtonSelector);
 
                 //Wait until the result is empty again
                 await WaitForEmptyResultAsync();
@@ -77,16 +64,16 @@ namespace Example.PageObjects
         public async Task<string?> WaitForNonEmptyResultAsync()
         {
             // Waits for the ResultLabelSelector value to be !== ""
-            await ResultLabelSelector.WaitForNonEmptyValue(await _page);
+            await _interactions.WaitForNonEmptyValue(ResultLabelSelector);
 
             // Gets the value attribute of the ResultLabelSelector
-            return await ResultLabelSelector.GetValueAttributeAsync(await _page);
+            return await _interactions.GetValueAttributeAsync(ResultLabelSelector);
         }
 
         public async Task WaitForEmptyResultAsync()
         {
             // Waits for the ResultLabelSelector value to be === ""
-            await ResultLabelSelector.WaitForEmptyValue(await _page);
+            await _interactions.WaitForEmptyValue(ResultLabelSelector);
         }
     }
 }
