56512dd192243d33a278b5b5c630daa50c697154
Jordan Western <j.western@tricentis.com>
2021-10-08 13:58:01 +0100

    shell to install windows app driver and execute

---
+++ .github/workflows/WindowsAppDriver.ps1
@@ -0,0 +1,9 @@
+﻿$windowsApplicationDriverPath = "https://github.com/microsoft/WinAppDriver/releases/download/v1.2.1/WindowsApplicationDriver_1.2.1.msi"
+
+cd\
+
+Invoke-WebRequest $windowsApplicationDriverPath -OutFile WindowsApplicationDriver_1.2.1.msi
+
+Start-Process msiexec.exe -Wait -argumentList "/i WindowsApplicationDriver_1.2.1.msi /quiet"
+
+Start-Process "Program Files (x86)\Windows Application Driver\WinAppDriver.exe"
