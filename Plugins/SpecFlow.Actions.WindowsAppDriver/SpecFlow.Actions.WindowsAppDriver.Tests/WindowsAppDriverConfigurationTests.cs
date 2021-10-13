159b492bc5686fe9d84219176cafe99af656473c
Jordan Western <j.western@tricentis.com>
2021-10-13 12:47:55 +0100

    Unit tests + removed one line of redundant code

---
+++ Plugins/SpecFlow.Actions.WindowsAppDriver/SpecFlow.Actions.WindowsAppDriver.Tests/WindowsAppDriverConfigurationTests.cs
@@ -0,0 +1,71 @@
+﻿using Moq;
+using SpecFlow.Actions.Configuration;
+using SpecFlow.Actions.WindowsAppDriver.Configuration;
+using Xunit;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace SpecFlow.Actions.WindowsAppDriver.Tests
+{
+    public class WindowsAppDriverConfigurationTests
+    {
+        private static WindowsAppDriverConfiguration GetAppDriverConfiguration(string specflowJsonContent)
+        {
+            var specflowActionJsonLoader = new Mock<ISpecFlowActionJsonLoader>();
+            specflowActionJsonLoader.Setup(m => m.Load()).Returns(specflowJsonContent);
+
+            return new WindowsAppDriverConfiguration(specflowActionJsonLoader.Object);
+        }
+
+        [Fact]
+        public void Capabilities_IsNull_If_Json_IsEmpty()
+        {
+            var specflowJsonContent = string.Empty;
+
+            var appDriverConfiguration = GetAppDriverConfiguration(specflowJsonContent);
+
+            appDriverConfiguration.Capabilities.Should().BeNull();
+        }
+
+        [Fact]
+        public void Capabilities_IsEmpty_If_ValuesNotProvided()
+        {
+            var specflowJsonContent = "{\"windowsAppDriver\": {\"capabilities\": {}}}";
+
+            var appDriverConfiguration = GetAppDriverConfiguration(specflowJsonContent);
+
+            appDriverConfiguration.Capabilities.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void WindowsAppDriverPath_IsNull_If_Json_IsEmpty()
+        {
+            var specflowJsonContent = string.Empty;
+
+            var appDriverConfiguration = GetAppDriverConfiguration(specflowJsonContent);
+
+            appDriverConfiguration.WindowsAppDriverPath.Should().BeNull();
+        }
+
+        [Fact]
+        public void EnableScreenshots_IsNull_If_Json_IsEmpty()
+        {
+            var specflowJsonContent = string.Empty;
+
+            var appDriverConfiguration = GetAppDriverConfiguration(specflowJsonContent);
+
+            appDriverConfiguration.EnableScreenshots.Should().BeNull();
+        }
+
+        [Fact]
+        public void LoadSpecFlowJson_Ignores_Casing()
+        {
+            var specflowJsonContent = "{\"WINDOWSAPPDRIVER\": {\"CAPABILITIES\": {\"APP\": \"path\"}}}";
+            var expected = new KeyValuePair<string, string>("APP", "path");
+
+            var appDriverConfiguration = GetAppDriverConfiguration(specflowJsonContent);
+
+            appDriverConfiguration.Capabilities!.Should().Contain(expected);
+        }
+    }
+}
