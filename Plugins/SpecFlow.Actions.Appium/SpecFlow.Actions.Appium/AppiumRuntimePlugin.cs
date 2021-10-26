d3bac5da6fa1a845dbc0ce803b89e04d152a0aee
Jordan Western <j.western@tricentis.com>
2021-10-26 09:28:00 +0100

    Added test project

---
+++ Plugins/SpecFlow.Actions.Appium/SpecFlow.Actions.Appium/AppiumRuntimePlugin.cs
@@ -0,0 +1,22 @@
+﻿using SpecFlow.Actions.Appium;
+using System;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly: RuntimePlugin(typeof(AppiumRuntimePlugin))]
+
+namespace SpecFlow.Actions.Appium
+{
+    public class AppiumRuntimePlugin : IRuntimePlugin
+    {
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            runtimePluginEvents.CustomizeScenarioDependencies += RuntimePluginEvents_CustomizeScenarioDependencies;
+        }
+
+        private void RuntimePluginEvents_CustomizeScenarioDependencies(object sender, CustomizeScenarioDependenciesEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
