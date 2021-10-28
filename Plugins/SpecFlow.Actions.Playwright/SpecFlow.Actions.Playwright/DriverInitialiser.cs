6c72eb31b45de25633a50a4e26ed8893eb575f39
kapros <kamil.prosowski@gmail.com>
2021-10-28 03:32:32 +0200

    add slowmo

--- Plugins/SpecFlow.Actions.Playwright/SpecFlow.Actions.Playwright/DriverInitialiser.cs
+++ Plugins/SpecFlow.Actions.Playwright/SpecFlow.Actions.Playwright/DriverInitialiser.cs
@@ -5,57 +5,57 @@ namespace SpecFlow.Actions.Playwright
 {
     public interface IDriverInitialiser
     {
-        Task<IBrowser> GetChromeDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true);
-        Task<IBrowser> GetEdgeDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true);
-        Task<IBrowser> GetFirefoxDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true);
-        Task<IBrowser> GetChromiumDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true);
-        Task<IBrowser> GetWebKitDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true);
+        Task<IBrowser> GetChromeDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null);
+        Task<IBrowser> GetEdgeDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null);
+        Task<IBrowser> GetFirefoxDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null);
+        Task<IBrowser> GetChromiumDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null);
+        Task<IBrowser> GetWebKitDriverAsync(string[]? args = null, float? timeout = DriverInitialiser.DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null);
     }
 
     public class DriverInitialiser : IDriverInitialiser
     {
         public const float DEFAULT_TIMEOUT = 30f;
 
-        public async Task<IBrowser> GetChromeDriverAsync(string[]? args = null, float? timeout = DEFAULT_TIMEOUT, bool? headless = true)
+        public async Task<IBrowser> GetChromeDriverAsync(string[]? args = null, float? timeout = DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null)
         {
-            var options = GetOptions(args, timeout, headless);
+            var options = GetOptions(args, timeout, headless, slowmo);
             options.Channel = "chrome";
 
             return await GetBrowserAsync(BrowserType.Chromium, options);
         }
 
-        public async Task<IBrowser> GetFirefoxDriverAsync(string[]? args = null, float? timeout = DEFAULT_TIMEOUT, bool? headless = true)
+        public async Task<IBrowser> GetFirefoxDriverAsync(string[]? args = null, float? timeout = DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null)
         {
-            var options = GetOptions(args, timeout, headless);
+            var options = GetOptions(args, timeout, headless, slowmo);
 
             return await GetBrowserAsync(BrowserType.Firefox, options);
         }
 
-        public async Task<IBrowser> GetEdgeDriverAsync(string[]? args = null, float? timeout = DEFAULT_TIMEOUT, bool? headless = true)
+        public async Task<IBrowser> GetEdgeDriverAsync(string[]? args = null, float? timeout = DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null)
         {
-            var options = GetOptions(args, timeout, headless);
+            var options = GetOptions(args, timeout, headless, slowmo);
             options.Channel = "msedge";
 
             return await GetBrowserAsync(BrowserType.Chromium, options);
         }
 
-        public async Task<IBrowser> GetChromiumDriverAsync(string[]? args, float? timeout = DEFAULT_TIMEOUT, bool? headless = true)
+        public async Task<IBrowser> GetChromiumDriverAsync(string[]? args, float? timeout = DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null)
         {
-            var options = GetOptions(args, timeout, headless);
+            var options = GetOptions(args, timeout, headless, slowmo);
 
             return await GetBrowserAsync(BrowserType.Chromium, options);
         }
 
-        public async Task<IBrowser> GetWebKitDriverAsync(string[]? args, float? timeout = DEFAULT_TIMEOUT, bool? headless = true)
+        public async Task<IBrowser> GetWebKitDriverAsync(string[]? args, float? timeout = DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null)
         {
-            var options = GetOptions(args, timeout, headless);
+            var options = GetOptions(args, timeout, headless, slowmo);
 
             return await GetBrowserAsync(BrowserType.Webkit, options);
         }
 
-        private BrowserTypeLaunchOptions GetOptions(string[]? args, float? timeout = DEFAULT_TIMEOUT, bool? headless = true)
+        private BrowserTypeLaunchOptions GetOptions(string[]? args, float? timeout = DEFAULT_TIMEOUT, bool? headless = true, float? slowmo = null)
             => new()
-            { Args = args, Timeout = ToMilliseconds(timeout), Headless = headless };
+            { Args = args, Timeout = ToMilliseconds(timeout), Headless = headless, SlowMo = slowmo };
 
         private async Task<IBrowser> GetBrowserAsync(string browserType, BrowserTypeLaunchOptions options)
         {
