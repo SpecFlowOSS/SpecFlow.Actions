f4a03ca7d499c293a79ce61e6a49fe2e0648d8ba
Jordan Western <j.western@tricentis.com>
2021-11-26 11:22:27 +0000

    fixes unit tests based on revised implementation

--- Plugins/SpecFlow.Actions.Selenium/SpecFlow.Actions.Selenium.Tests/BrowserDriverTests.cs
+++ Plugins/SpecFlow.Actions.Selenium/SpecFlow.Actions.Selenium.Tests/BrowserDriverTests.cs
@@ -6,6 +6,7 @@ using Xunit;
 using SpecFlow.Actions.Selenium.Configuration;
 using SpecFlow.Actions.Selenium.Factories;
 using SpecFlow.Actions.Selenium.Enums;
+using BoDi;
 
 namespace SpecFlow.Actions.Selenium.Tests
 {
@@ -15,7 +16,7 @@ namespace SpecFlow.Actions.Selenium.Tests
         {
             public Lazy<IWebDriver> CurrentWebDriverLazy => _currentWebDriverLazy;
 
-            public BrowserDriverAccessor(ISeleniumConfiguration seleniumConfiguration, IDriverFactory browserFactory) : base(seleniumConfiguration, browserFactory)
+            public BrowserDriverAccessor(ISeleniumConfiguration seleniumConfiguration, IObjectContainer objectContainer) : base(seleniumConfiguration, objectContainer)
             {
             }
         }
@@ -24,9 +25,9 @@ namespace SpecFlow.Actions.Selenium.Tests
         public void Current_NotInstantiatedAfterCreation()
         {
             var seleniumConfigurationMock = new Mock<ISeleniumConfiguration>();
-            var driverFactoryMock = new Mock<IDriverFactory>();
+            var objectContainerMock = new Mock<IObjectContainer>();
 
-            var target = new BrowserDriverAccessor(seleniumConfigurationMock.Object, driverFactoryMock.Object);
+            var target = new BrowserDriverAccessor(seleniumConfigurationMock.Object, objectContainerMock.Object);
 
             target.CurrentWebDriverLazy.IsValueCreated.Should().BeFalse();
         }
@@ -35,13 +36,22 @@ namespace SpecFlow.Actions.Selenium.Tests
         public void Current_AfterAccessing_Instantiated()
         {
             var seleniumConfigurationMock = new Mock<ISeleniumConfiguration>();
-            seleniumConfigurationMock.Setup(m => m.Targets).Returns(new TargetConfiguration[] { new TargetConfiguration() });
+            seleniumConfigurationMock
+                .Setup(x => x.Targets)
+                .Returns(new TargetConfiguration[] { new TargetConfiguration { Browser = Browser.Noop } });
 
-            var driverFactoryMock = new Mock<IDriverFactory>();
-            driverFactoryMock.Setup(m => m.GetDriver(seleniumConfigurationMock.Object.Targets[0])).Returns(new NoopWebdriver());
+            seleniumConfigurationMock
+                .Setup(x => x.TestPlatform)
+                .Returns("local");
 
-            var target = new BrowserDriverAccessor(seleniumConfigurationMock.Object, driverFactoryMock.Object);
+            var driverFactoryMock = new Mock<LocalDriverFactory>();
 
+            var objectContainerMock = new Mock<IObjectContainer>();
+            objectContainerMock
+                .Setup(x => x.Resolve<IDriverFactory>(seleniumConfigurationMock.Object.TestPlatform))
+                .Returns(driverFactoryMock.Object);
+
+            var target = new BrowserDriverAccessor(seleniumConfigurationMock.Object, objectContainerMock.Object);
 
             var webdriver = target.Current;
 
