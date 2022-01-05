53a62b3cbd2bd1236251ab80f1dfc543ae1f9547
Jordan Western <j.western@tricentis.com>
2022-01-05 12:52:36 +0000

    implements code from spike

---
+++ Plugins/SpecFlow.Actions.Selenium/Selenium.Targets.Generation/MultiFeatureGenerator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using TechTalk.SpecFlow.Generator.Generation;
+using TechTalk.SpecFlow.Generator.UnitTestConverter;
+using TechTalk.SpecFlow.Parser;
+
+namespace Selenium.Targets.Generation
+{
+    internal class MultiFeatureGenerator : IFeatureGenerator
+    {
+        private readonly IFeatureGenerator _defaultFeatureGenerator;
+
+        private readonly KeyValuePair<string, IFeatureGenerator>[] _featureGenerators;
+
+        private readonly List<string> _unitTestProviderTags = new List<string> { "xunit", "mstest", "nunit3" };
+
+        public MultiFeatureGenerator(IEnumerable<KeyValuePair<string, IFeatureGenerator>> featureGenerators, IFeatureGenerator defaultFeatureGenerator)
+        {
+            _defaultFeatureGenerator = defaultFeatureGenerator;
+            _featureGenerators = featureGenerators.ToArray();
+
+            foreach (var featureGenerator in _featureGenerators)
+            {
+                if (featureGenerator.Value is UnitTestFeatureGenerator unitTestFeatureGenerator)
+                {
+                    unitTestFeatureGenerator.TestClassNameFormat += $"_{featureGenerator.Key}";
+                }
+            }
+        }
+
+        public CodeNamespace GenerateUnitTestFixture(SpecFlowDocument specFlowDocument, string testClassName, string targetNamespace)
+        {
+            CodeNamespace? result = null;
+            bool onlyFullframework = false;
+
+            var specFlowFeature = specFlowDocument.SpecFlowFeature;
+            bool onlyDotNetCore = false;
+            if (specFlowFeature.HasTags())
+            {
+                if (specFlowFeature.Tags.Where(t => t.Name == "@SingleTestConfiguration").Any())
+                {
+                    return _defaultFeatureGenerator.GenerateUnitTestFixture(specFlowDocument, testClassName, targetNamespace);
+                }
+
+                onlyFullframework = HasFeatureTag(specFlowFeature, "@fullframework");
+                onlyDotNetCore = HasFeatureTag(specFlowFeature, "@dotnetcore");
+            }
+
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                onlyFullframework = false;
+                onlyDotNetCore = true;
+            }
+
+            var tagsOfFeature = specFlowFeature.Tags.Select(t => t.Name);
+            var unitTestProviders = tagsOfFeature.Where(t => _unitTestProviderTags.Where(utpt => string.Compare(t, "@" + utpt, StringComparison.CurrentCultureIgnoreCase) == 0).Any());
+
+            foreach (var featureGenerator in _featureGenerators)
+            {
+                var featureGeneratorResult = featureGenerator.Value.GenerateUnitTestFixture(specFlowDocument, testClassName, targetNamespace);
+
+                if (result == null)
+                {
+                    result = featureGeneratorResult;
+                }
+                else
+                {
+                    foreach (CodeTypeDeclaration type in featureGeneratorResult.Types)
+                    {
+                        result.Types.Add(type);
+                    }
+                }
+            }
+
+            if (result == null)
+            {
+                result = new CodeNamespace(targetNamespace);
+            }
+
+            return result;
+        }
+
+        private bool HasFeatureTag(SpecFlowFeature specFlowFeature, string tag)
+        {
+            return specFlowFeature.Tags.Any(t => string.Compare(t.Name, tag, StringComparison.CurrentCultureIgnoreCase) == 0);
+        }
+    }
+}
---
+++ Plugins/SpecFlow.Actions.Selenium/Selenium.Targets.Generation/MultiFeatureGeneratorProvider.cs
@@ -0,0 +1,48 @@
+﻿using BoDi;
+using System.Collections.Generic;
+using TechTalk.SpecFlow.Configuration;
+using TechTalk.SpecFlow.Generator.CodeDom;
+using TechTalk.SpecFlow.Generator.Generation;
+using TechTalk.SpecFlow.Generator.UnitTestConverter;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Parser;
+
+namespace Selenium.Targets.Generation
+{
+    internal class MultiFeatureGeneratorProvider : IFeatureGeneratorProvider
+    {
+        private readonly MultiFeatureGenerator _multiFeatureGenerator;
+
+        public MultiFeatureGeneratorProvider(IObjectContainer container)
+        {
+            var list = new List<string>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add($"Generated_Feature_{i}");
+            }
+
+            var featureGenerators = new List<KeyValuePair<string, IFeatureGenerator>>();
+
+            foreach (var combination in list)
+            {
+                var combinationFeatureGenerator = new UnitTestFeatureGenerator(container.Resolve<IUnitTestGeneratorProvider>(), container.Resolve<CodeDomHelper>(), container.Resolve<SpecFlowConfiguration>(), container.Resolve<IDecoratorRegistry>());
+                featureGenerators.Add(new KeyValuePair<string, IFeatureGenerator>(combination, combinationFeatureGenerator));
+            }
+
+            _multiFeatureGenerator = new MultiFeatureGenerator(featureGenerators, new UnitTestFeatureGenerator(container.Resolve<IUnitTestGeneratorProvider>(), container.Resolve<CodeDomHelper>(), container.Resolve<SpecFlowConfiguration>(), container.Resolve<IDecoratorRegistry>()));
+        }
+
+        public bool CanGenerate(SpecFlowDocument specFlowDocument)
+        {
+            return true;
+        }
+
+        public IFeatureGenerator CreateGenerator(SpecFlowDocument specFlowDocument)
+        {
+            return _multiFeatureGenerator;
+        }
+
+        public int Priority => PriorityValues.Normal;
+    }
+}
