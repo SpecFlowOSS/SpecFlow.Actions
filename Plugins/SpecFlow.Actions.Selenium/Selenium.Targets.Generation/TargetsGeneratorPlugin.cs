e3b947e9be830bf408142b455d0d9260fec46168
Jordan Western <j.western@tricentis.com>
2022-01-06 13:21:38 +0000

    nuget package can be consumed locally and executes plugin, issue with config read from plugin

--- Plugins/SpecFlow.Actions.Selenium/Selenium.Targets.Generation/TargetsGeneratorPlugin.cs
+++ Plugins/SpecFlow.Actions.Selenium/Selenium.Targets.Generation/TargetsGeneratorPlugin.cs
@@ -1,7 +1,11 @@
 ﻿using Selenium.Targets.Generation;
+using SpecFlow.Actions.Configuration;
+using System;
+using System.Diagnostics;
 using TechTalk.SpecFlow.Generator.Plugins;
 using TechTalk.SpecFlow.Generator.UnitTestConverter;
 using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.Plugins;
 using TechTalk.SpecFlow.UnitTestProvider;
 
 [assembly: GeneratorPlugin(typeof(TargetsGeneratorPlugin))]
@@ -12,10 +16,19 @@ namespace Selenium.Targets.Generation
         public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
         {
             generatorPluginEvents.CustomizeDependencies += GeneratorPluginEvents_CustomizeDependencies;
+            generatorPluginEvents.RegisterDependencies += GeneratorPluginEvents_RegisterDependencies;
+        }
+
+        private void GeneratorPluginEvents_RegisterDependencies(object sender, RegisterDependenciesEventArgs e)
+        {
+            e.ObjectContainer.RegisterTypeAs<SpecFlowActionJsonLoader, ISpecFlowActionJsonLoader>();
+            e.ObjectContainer.RegisterTypeAs<SpecFlowActionJsonLocator, ISpecFlowActionJsonLocator>();
         }
 
         private void GeneratorPluginEvents_CustomizeDependencies(object sender, CustomizeDependenciesEventArgs e)
         {
+            e.ObjectContainer.RegisterTypeAs<SpecFlowActionJsonLoader, ISpecFlowActionJsonLoader>();
+            e.ObjectContainer.RegisterTypeAs<SpecFlowActionJsonLocator, ISpecFlowActionJsonLocator>();
             e.ObjectContainer.RegisterTypeAs<MultiFeatureGeneratorProvider, IFeatureGeneratorProvider>();
         }
     }
